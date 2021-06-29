d1630a1fa55ca8c9ef6308523abd470c87659c63
Ben-Costa <bc234cc@gmail.comm>
2021-06-28 20:26:51 -0500

    Final version of the home page. Base of the flask. Before the creation of the base.html template and the creation of the other flask specfic OOP functions.

---
+++ POFWebpage/routes.py
@@ -0,0 +1,8 @@
+from POFWebpage import app
+from flask import render_template, redirect, url_for
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('pof_home.html')
+
