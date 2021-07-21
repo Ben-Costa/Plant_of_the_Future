2ef1c87be353d7c9f7ddfdd133c7e64ac65bb397
Ben-Costa <bc234cc@gmail.comm>
2021-07-21 03:18:49 +0000

    added in about, login, home pages. Used base template as base for pages. Need to make pages look better and remove and add comments

--- POFWebpage/routes.py
+++ POFWebpage/routes.py
@@ -4,5 +4,16 @@ from flask import render_template, redirect, url_for
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('base.html')
+    return render_template('pof_home.html')
 
+@app.route('/about')
+def about_page():
+    return render_template('pof_about.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('pof_contact.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('pof_login.html')
