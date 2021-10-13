b3f65296083eddbd57dae1c3b213669100dcca62
Ben-PC-2 <bc234cc@gmail.com>
2021-10-12 20:05:47 -0400

    testing git ignore

--- POFWebpage/User/user_class.py
+++ POFWebpage/User/user_class.py
@@ -2,15 +2,37 @@ from flask import Flask, jsonify
 
 class User:
 
-    def ceate_user(self):
+    def __init__(self, first_name, last_name, email, password, organization):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email    
+        self.first_name = first_name
+        self.password = password    
+        self.organization = organization    
 
-        User = {
-            "_id": "",
-            "email": "",
-            "password": "",
-            "name": "",
-            "organization": ""
+        #pass to database to make sure doesnt exist
+
+        #if exists return error
+
+        #if not return user successfully created
+    
+    
+    def user_to_json(self):
+
+        user_Dict = {
+            "email": self.email,
+            "password": self.password,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "organization": self.organization
          }
 
-        return jsonify(User), 200
+        return jsonify(user_Dict), 200
 
+
+
+    def json_to_user(self):
+        pass
+
+    def check_existance(self):
+        pass
